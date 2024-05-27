af520d9743ef1a198ab70f6809a62e5c35c85fa3
0watcher <patrykstruzek17@gmail.com>
2024-05-27 18:24:01 +0200

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,38 @@ use ratatui::{
 };
 use regex::Regex;
 
+/// Creates a `Line` from the given `line` argument and adds `highlight_style` to `Spans` that match the pattern.
+///
+/// # Arguments
+///
+/// * `line` - A string slice that holds the line of text to be highlighted.
+/// * `pattern` - A regular expression pattern to match the text that needs to be highlighted.
+/// * `highlight_style` - The style to be applied to the matching text.
+///
+/// # Example
+///
+/// ```
+/// use tui_pattern_highlighter::highlight_line;
+/// use ratatui::{
+///     style::{Color, Style},
+///     text::{Line, Span},
+/// };
+///
+/// let line = "Hi @buddy";
+/// let pattern = r"@\w+";
+/// let highlight_style = Style::new().bg(Color::Blue);
+///
+/// let expected_line = Line::from(vec![
+///     Span::from("Hi "),
+///     Span::from("@buddy").style(Style::new().bg(Color::Blue)),
+/// ]);
+///
+/// assert_eq!(highlight_line(line, pattern, highlight_style), expected_line);
+/// ```
+///
+/// # Panics
+///
+/// The function may panic if the provided pattern is an invalid regular expression.
 pub fn highlight_line<'a>(line: &'a str, pattern: &str, highlight_style: Style) -> Line<'a> {
     let mut highlighted_line = Line::default();
 
@@ -14,7 +46,9 @@ pub fn highlight_line<'a>(line: &'a str, pattern: &str, highlight_style: Style) 
 
     if find_iter.last().is_some() {
         for m in reg.find_iter(&line) {
-            highlighted_line.push_span(Span::from(&line[last_index..m.start()]));
+            if m.start() != 0 {
+                highlighted_line.push_span(Span::from(&line[last_index..m.start()]));
+            }
             highlighted_line.push_span(Span::from(m.as_str()).style(highlight_style));
             last_index = m.end();
         }
@@ -27,6 +61,45 @@ pub fn highlight_line<'a>(line: &'a str, pattern: &str, highlight_style: Style) 
     highlighted_line
 }
 
+/// Creates `Text` from the given `line` argument and adds `highlight_style` to `Spans` that match the pattern.
+/// When the '\n' character is encountered, a new `Line` begins.
+///
+/// # Arguments
+///
+/// * `text` - A string slice that holds the text to be highlighted.
+/// * `pattern` - A regular expression pattern to match the text that needs to be highlighted.
+/// * `highlight_style` - The style to be applied to the matching text.
+///
+/// # Example
+///
+/// ```
+/// use tui_pattern_highlighter::highlight_text;
+/// use ratatui::{
+///     style::{Color, Style},
+///     text::{Line, Span, Text},
+/// };
+///
+/// let text = "Hi @buddy\n@stranger hello";
+/// let pattern = r"@\w+";
+/// let highlight_style = Style::new().bg(Color::Blue);
+///
+/// let expected_text = Text::from(vec![
+///     Line::from(vec![
+///         Span::from("Hi "),
+///         Span::from("@buddy").style(Style::new().bg(Color::Blue)),
+///     ]),
+///     Line::from(vec![
+///         Span::from("@stranger").style(Style::new().bg(Color::Blue)),
+///         Span::from(" hello"),
+///     ]),
+/// ]);
+///
+/// assert_eq!(highlight_text(text, pattern, highlight_style), expected_text);
+/// ```
+///
+/// # Panics
+///
+/// The function may panic if the provided pattern is an invalid regular expression.
 pub fn highlight_text<'a>(line: &'a str, pattern: &str, highlight_style: Style) -> Text<'a> {
     let mut highlighted_text = Text::default();
 
@@ -93,8 +166,6 @@ mod tests {
                 Span::from(" is ?"),
             ]),
         ]);
-
-        assert_eq!(returned_text.lines[1], text.lines[1]);
 
         assert_eq!(returned_text, text);
     }
